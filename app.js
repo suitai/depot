01626d871b2a139651949b63af64e7cf90c59607
Sui, Tai <suitai@outlook.jp>
2019-02-22 00:38:42 +0900

    /ftp ファイルを公開

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var serveIndex = require('serve-index')
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/upload', uploadRouter);
+app.use('/ftp', express.static('public/ftp'), serveIndex('public/ftp', {'icons': true}))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/upload.js
+++ routes/upload.js
@@ -5,7 +5,7 @@ var multer = require('multer')
 var upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads')
+      cb(null, 'public/ftp')
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
