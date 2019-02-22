4581340b12a4e3d50931cb78c3188be5ebafc928
Sui, Tai <suitai@outlook.jp>
2019-02-23 01:42:34 +0900

    default.json から Category を選択

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('config');
+var category = config.get('Category');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', category: category });
 });
 
 module.exports = router;
