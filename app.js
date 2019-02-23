5d2bf14357d9475332319cbba5294ce070f86bdd
Sui, Tai <suitai@outlook.jp>
2019-02-23 20:41:08 +0900

    書式を修正

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var serveIndex = require('serve-index')
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const serveIndex = require('serve-index')
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var uploadRouter = require('./routes/upload');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const uploadRouter = require('./routes/upload');
 
 const uploadDir = process.env.UPLOAD_DIR
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,12 +31,12 @@ app.use('/upload', uploadRouter);
 app.use('/ftp', express.static(uploadDir), serveIndex(uploadDir, {'icons': true}))
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
