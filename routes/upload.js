1fe68269ba7594cb6c779423564c5cdcebba5fe8
Sui, Tai <suitai@outlook.jp>
2019-02-24 12:00:30 +0900

    name を dir に短縮

--- routes/upload.js
+++ routes/upload.js
@@ -16,14 +16,13 @@ const upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       const extension = path.extname(file.originalname);
-
       for (type in filetypes) {
         if (filetypes[type]['extension'].includes(extension)) {
           filetype = type;
           typeSet.add(type);
         }
       }
-      const dir = path.join(uploadDir, filetype, req.body.directory);
+      const dir = path.join(uploadDir, filetype, req.body.dir);
       if (!fs.existsSync(dir)) {
         fs.mkdirsSync(dir);
       }
@@ -36,11 +35,12 @@ const upload = multer({
 })
 
 router.post('/', upload.array('file', 12), (req, res, next) => {
+  console.log(`directory: ${req.body.dir}`);
   req.files.forEach((file) => {
     console.log(`file: ${file.path}`);
   });
   typeSet.forEach((type) => {
-    const execOpt = {cwd: uploadDir, env: {repodir: path.join(type, req.body.directory.split(path.sep)[0])}};
+    const execOpt = {cwd: uploadDir, env: {repodir: path.join(type, req.body.dir.split(path.sep)[0])}};
     if ('script' in filetypes[type]) {
       console.log(`exec: ${filetypes[type]['script']}`)
       exec(filetypes[type]['script'], execOpt, (error, stdout, stderr) => {
