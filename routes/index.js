256d29fa3cdc056a27fe07bbf53177f1f951c642
Sui, Tai <suitai@outlook.jp>
2019-02-24 14:46:46 +0900

    curl コマンドを表示

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ const config = require('config');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Depot', directory: config.get('Directory') });
+  res.render('index', {
+    title: 'Depot',
+    directory: config.get('Directory'),
+    url: req.protocol + '://' + req.headers.host + req.url
+  });
 });
 
 module.exports = router;
