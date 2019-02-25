703ddff3ba02e37f54810bba63d6558e2a615d32
Sui, Tai <suitai@outlook.jp>
2019-02-25 21:33:27 +0900

    upload してから move

--- routes/upload.js
+++ routes/upload.js
@@ -8,53 +8,25 @@ const config = require('config');
 const { exec } = require('child_process');
 
 const uploadDir = process.env.UPLOAD_DIR
-const filetypes = config.get('Filetype');
-let typeSet = new Set();
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      const extension = path.extname(file.originalname);
-      let filetype = 'Others';
-      for (type in filetypes) {
-        if (filetypes[type]['extension'].includes(extension)) {
-          filetype = type;
-          typeSet.add(type);
-        }
-      }
-      console.log(`filetype: ${filetype}`);
-      const dir = path.join(uploadDir, filetype, req.body.dir);
-      if (!fs.existsSync(dir)) {
-        fs.mkdirsSync(dir);
-      }
-      cb(null, dir);
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
-    }
-  })
-})
+const tmpDir = path.join(uploadDir, 'tmp');
+if (!fs.existsSync(tmpDir)) {
+  fs.mkdirsSync(tmpDir);
+}
+const upload = multer({ dest: tmpDir });
 
 router.post('/', upload.array('file', 12), (req, res, next) => {
   console.log(`directory: ${req.body.dir}`);
+  const dir = path.join(uploadDir, req.body.dir);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirsSync(dir);
+  }
   req.files.forEach((file) => {
-    console.log(`file: ${file.path}`);
+    const newpath = path.join(dir, file.originalname);
+    console.log(`file: ${newpath}`);
+    fs.rename(file.path, newpath, (err) => {
+      if (err) throw err;
+    });
   });
-  typeSet.forEach((type) => {
-    const execOpt = {cwd: uploadDir, env: {repodir: path.join(type, req.body.dir.split(path.sep)[0])}};
-    if ('script' in filetypes[type]) {
-      console.log(`exec: ${filetypes[type]['script']}`)
-      exec(filetypes[type]['script'], execOpt, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      });
-    }
-  });
-  typeSet.clear();
   res.send('success!');
 });
 
