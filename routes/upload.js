865474c23a501f066cd018a8eabe6ba45e8c5364
Sui, Tai <suitai@outlook.jp>
2019-02-25 23:54:02 +0900

    rename 設定を追加

--- routes/upload.js
+++ routes/upload.js
@@ -15,16 +15,50 @@ if (!fs.existsSync(tmpDir)) {
 const upload = multer({ dest: tmpDir });
 
 router.post('/', upload.array('file', 12), (req, res, next) => {
+  const operates = config.get('Operate');
+  const newdir = path.join(uploadDir, req.body.dir);
   console.log(`directory: ${req.body.dir}`);
-  const dir = path.join(uploadDir, req.body.dir);
-  if (!fs.existsSync(dir)) {
-    fs.mkdirsSync(dir);
+  if (!fs.existsSync(newdir)) {
+    fs.mkdirsSync(newdir);
   }
   req.files.forEach((file) => {
-    const newpath = path.join(dir, file.originalname);
+    const newpath = path.join(newdir, file.originalname);
     console.log(`file: ${newpath}`);
     fs.rename(file.path, newpath, (err) => {
-      if (err) throw err;
+      if (err) {
+        console.error(`rename error: ${err}`);
+        return;
+      }
+    });
+    operates.forEach((operate) => {
+      if (file.originalname.match(operate.regex)) {
+        const execOpt = {
+          cwd: uploadDir,
+          env: {
+            filename: file.originalname,
+            dir: newdir
+          }
+        };
+        if ('rename' in operate) {
+          exec(`echo ${operate.rename}`, execOpt, (err, stdout, stderr) => {
+            if (err) {
+              console.error(`exec error: ${err}`);
+              return;
+            }
+            console.log(`rename: ${stdout}`)
+            renameDir = path.dirname(stdout);
+            if (!fs.existsSync(renameDir)) {
+              fs.mkdirsSync(renameDir);
+            }
+            fs.rename(newpath, path.join(newdir, stdout), (err) => {
+              if (err) {
+                console.error(`rename error: ${err}`);
+                return;
+              }
+            });
+          });
+        }
+      }
     });
   });
   res.send('success!');
