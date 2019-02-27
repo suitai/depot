430ef02c22b0746700668ee6559cab0c38c3d4b1
Sui, Tai <suitai@outlook.jp>
2019-02-27 10:57:39 +0900

    整理

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
 const bodyParser = require('body-parser');
 const serveIndex = require('serve-index')
-const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const uploadRouter = require('./routes/upload');
 
-const uploadDir = process.env.UPLOAD_DIR
+const uploadDir = process.env.UPLOAD_DIR;
 
 const app = express();
 
@@ -21,8 +22,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/upload', uploadRouter);
