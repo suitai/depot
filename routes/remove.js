7502617ff9bd39121e1fcdc7def72210c8c43699
Matsuyama, Kazuhiro <k.matsuyama@jp.fujitsu.com>
2019-08-05 13:22:47 +0900

    remove 時にも createrepo を実施

--- routes/remove.js
+++ routes/remove.js
@@ -3,13 +3,17 @@ const router = express.Router();
 
 const fs = require('fs');
 const path = require('path');
+const config = require('config');
+const childProcess = require('child_process');
 
 const uploadDir = process.env.UPLOAD_DIR
 
 router.get('/', (req, res, next) => {
+  const operates = config.get('Operate.Remove');
   const reqpath = req.query.path;
   const filepath = path.join(uploadDir, req.query.path);
   const dirname = path.dirname(filepath);
+
   if (path.resolve(filepath).indexOf(path.resolve(uploadDir)) != 0 ) {
     res.send({error: `${reqpath} is Invalid.`});
   }
@@ -24,6 +28,39 @@ router.get('/', (req, res, next) => {
       console.error(`remove error: ${err}`);
     }
   });
+
+  for (operate of operates) {
+    let match = new Array();
+    if ('match' in operate) {
+      match = reqpath.match(operate.match);
+      if (!match) {
+        continue;
+      }
+      isMatch = true;
+    }
+    let execOpt = {
+      cwd: uploadDir,
+      env: {
+        filepath: reqpath,
+        dirname: path.dirname(reqpath),
+        match_0: match[0],
+        match_1: match[1]
+      }
+    };
+    if ('post' in operate) {
+      console.log(`post: ${operate.post}`);
+      let postStdout = childProcess.execSync(operate.post, execOpt).toString().trim();
+      postStdout = postStdout.replace(/\n/g, '\nstdout: ')
+      console.log(`stdout: ${postStdout}`);
+    }
+    if (! 'break' in operate) {
+      break;
+    } else {
+      if (operate.break) {
+        break;
+      }
+    }
+  }
   res.format({
     text: function(){
       res.send(`${reqpath} Remove!\n`);
