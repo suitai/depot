fe5cde224170d8d930aab8f8c6afcb65a9b35391
Sui, Tai <suitai@outlook.jp>
2019-09-21 23:50:00 +0900

    queue を lib に移行

--- routes/upload.js
+++ routes/upload.js
@@ -5,8 +5,8 @@ const multer = require('multer');
 const fs = require('fs-extra');
 const path = require('path');
 const config = require('config');
-const childProcess = require('child_process');
-const async = require('async');
+
+const queue = require('../lib/queue.js');
 
 const uploadDir = process.env.UPLOAD_DIR;
 const tmpDir = path.join(uploadDir, '.tmp');
@@ -14,48 +14,6 @@ if (!fs.existsSync(tmpDir)) {
   fs.mkdirsSync(tmpDir);
 }
 const upload = multer({ dest: tmpDir });
-
-const filemove = (oldpath, newpath, done) => {
-  const newdir = path.dirname(newpath);
-  if (!fs.existsSync(newdir)) {
-    fs.mkdirsSync(newdir);
-  }
-  fs.rename(oldpath, newpath, (err) => {
-    if (err) return done(err);
-  });
-};
-
-const pluginQueue = async.queue((data, callback) => {
-  const execOpt = {
-    cwd: uploadDir,
-    env: {
-      dest: data.dest,
-      filename: data.filename,
-      filepath: path.relative(uploadDir, data.filepath),
-      dirname: path.relative(uploadDir, path.dirname(data.filepath)),
-      match_0: data.match[0],
-      match_1: data.match[1]
-    }
-  };
-  if ('rename' in data.operate) {
-    let renamePath = childProcess.execSync(`echo ${data.operate.rename}`, execOpt).toString().trim();
-    renamePath = path.join(uploadDir, renamePath);
-    console.log(`rename: ${renamePath}`);
-    filemove(data.filepath, renamePath, (err) => {
-      console.error(`rename: ${err}`);
-      callback(err);
-    });
-    execOpt.env.filepath = path.relative(uploadDir, renamePath);
-    execOpt.env.dirname = path.relative(uploadDir, path.dirname(renamePath));
-  }
-  if ('post' in data.operate) {
-    console.log(`post: ${data.operate.post}`);
-    let postStdout = childProcess.execSync(data.operate.post, execOpt).toString().trim();
-    postStdout = postStdout.replace(/\n/g, '\nstdout: ');
-    console.log(`stdout: ${postStdout}`);
-  }
-  callback(null);
-});
 
 router.post('/', upload.array('file', 12), (req, res) => {
   const operates = config.get('Operate.Upload');
@@ -70,7 +28,7 @@ router.post('/', upload.array('file', 12), (req, res) => {
         }
         isMatch = true;
       }
-      pluginQueue.push({
+      queue.plugin.push({
         dest: req.body.dest,
         filename: file.originalname,
         filepath: file.path,
