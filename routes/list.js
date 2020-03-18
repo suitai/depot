54de5bd772dbea6b644aed519d47d4412216452b
Matsuyama, Kazuhiro <k.matsuyama@jp.fujitsu.com>
2020-03-18 16:39:52 +0900

    search 結果をファイルに保存

--- routes/list.js
+++ routes/list.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs-extra');
 const config = require('config');
+const path = require('path');
 
 const search = require('../lib/search.js');
+
+const uploadDir = process.env.UPLOAD_DIR;
+const cacheDir = path.join(uploadDir, '.cache');
+if (!fs.existsSync(cacheDir)) {
+  fs.mkdirsSync(cacheDir);
+}
+const listFile = path.join(cacheDir, 'files.json')
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -13,14 +22,23 @@ router.get('/', (req, res) => {
     baseUrl: req.protocol + '://' + req.headers.host + process.env.DOWNLOAD_DIR + '/',
     ignore: config.get('List').ignore
   };
-  search.files(uploadDir, option, (err, list) => {
-    if (err) throw err;
-    list.sort((a, b) => {
-      if (a.path < b.path) return -1;
-      if (a.path > b.path) return 1;
-      return 0;
-    });
-    res.send(list);
+  fs.stat(listFile, (err, stat) => {
+    if (err == null) {
+      res.redirect(option.baseUrl + '.cache/files.json')
+    } else {
+      search.files(uploadDir, option, (err, list) => {
+        if (err) throw err;
+        list.sort((a, b) => {
+          if (a.path < b.path) return -1;
+          if (a.path > b.path) return 1;
+          return 0;
+        });
+        fs.writeFile(listFile, JSON.stringify(list, undefined, 4), 'utf8', (err) => {
+          if (err) throw err;
+        });
+        res.send(list);
+      });
+    }
   });
 });
 
