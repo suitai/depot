3a4959b91af9a0e7501f64201cdcefa9e2e637a7
Sui, Tai <suitai@outlook.jp>
2020-04-04 13:17:12 +0900

    fileList 作成関数を search.js に移動

--- routes/list.js
+++ routes/list.js
@@ -8,17 +8,17 @@ const queue = require('../lib/queue.js');
 
 const uploadDir = process.env.UPLOAD_DIR;
 const downloadDir = process.env.DOWNLOAD_DIR;
-const listFile = path.join(uploadDir, '.cache', 'files.json');
-const waitTime = 1000;
 
 /* GET home page. */
 router.get('/', (req, res) => {
   const baseUrl = req.protocol + '://' + req.headers.host + downloadDir + '/';
+  const listFile = path.join(uploadDir, '.cache/files.json');
+  const waitTime = 1000;
   fs.access(listFile, (err) => {
     if (err == null) {
       res.redirect(baseUrl + '.cache/files.json');
     } else {
-      queue.file.push({'operate': {'create': 'fileList'}});
+      queue.file.push({operate: {create: 'fileList'}});
       console.log(`sleep: ${waitTime}`);
       setTimeout(() => {
         res.redirect(baseUrl + '.cache/files.json');
