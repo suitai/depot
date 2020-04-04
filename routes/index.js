b201bcb489c40e91a6a839ec448e4f44fb0f524c
Sui, Tai <suitai@outlook.jp>
2020-04-04 12:22:24 +0900

    不要な Directory 設定を変更

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const config = require('config');
-
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', {
     title: process.env.TITLE,
     download: process.env.DOWNLOAD_DIR,
-    directory: config.get('Directory'),
     url: req.protocol + '://' + req.headers.host + req.url
   });
 });
