ea3426f8687989e9a5eb081b9de9ffc6b4e4d679
Sui, Tai <suitai@outlook.jp>
2020-04-04 10:30:52 +0900

    queue を使って fileList を作成

--- routes/list.js
+++ routes/list.js
@@ -2,41 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 const fs = require('fs-extra');
-const config = require('config');
 const path = require('path');
 
-const search = require('../lib/search.js');
+const queue = require('../lib/queue.js');
 
 const uploadDir = process.env.UPLOAD_DIR;
 const downloadDir = process.env.DOWNLOAD_DIR;
-const cacheDir = path.join(uploadDir, '.cache');
-if (!fs.existsSync(cacheDir)) {
-  fs.mkdirsSync(cacheDir);
-}
-const listFile = path.join(cacheDir, 'files.json')
+const listFile = path.join(uploadDir, '.cache', 'files.json');
+const waitTime = 1000;
 
 /* GET home page. */
 router.get('/', (req, res) => {
   const baseUrl = req.protocol + '://' + req.headers.host + downloadDir + '/';
-  const option = {
-    ignore: config.get('List').ignore
-  };
-  fs.stat(listFile, (err, stat) => {
+  fs.access(listFile, (err) => {
     if (err == null) {
-      res.redirect(baseUrl + '.cache/files.json')
+      res.redirect(baseUrl + '.cache/files.json');
     } else {
-      search.files(uploadDir, option, (err, list) => {
-        if (err) throw err;
-        list.sort((a, b) => {
-          if (a.path < b.path) return -1;
-          if (a.path > b.path) return 1;
-          return 0;
-        });
-        fs.writeFile(listFile, JSON.stringify(list, undefined, 4), 'utf8', (err) => {
-          if (err) throw err;
-        });
-        res.send(list);
-      });
+      queue.file.push({'operate': {'create': 'fileList'}});
+      console.log(`sleep: ${waitTime}`);
+      setTimeout(() => {
+        res.redirect(baseUrl + '.cache/files.json');
+      }, waitTime);
     }
   });
 });
