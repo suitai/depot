0967133a81d76f825de966ca5890cba28935a7ca
Matsuyama, Kazuhiro <k.matsuyama@jp.fujitsu.com>
2020-04-07 19:49:17 +0900

    remove の path 誤りを修正

--- routes/remove.js
+++ routes/remove.js
@@ -8,11 +8,12 @@ const config = require('config');
 const queue = require('../lib/queue.js');
 
 const uploadDir = process.env.UPLOAD_DIR;
+const downloadDir = process.env.DOWNLOAD_DIR;
 
 router.get('/', (req, res) => {
   const operates = config.get('Operate.Remove');
   const reqpath = req.query.path;
-  const filepath = path.join(uploadDir, req.query.path);
+  const filepath = path.join(uploadDir, reqpath.substr(downloadDir.length));
 
   if (path.resolve(filepath).indexOf(path.resolve(uploadDir)) != 0 ) {
     console.log(`${reqpath} is Invalid.`);
